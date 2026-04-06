2ebf72d09021100ad4b30d155e84e692c2a469b9
YongliHe23 <yonglihe@umich.edu>
2026-04-06 11:56:59 -0400

    move target_Mss from @SpinArray to +utils

--- @SpinArray/SpinArray.m
+++ @SpinArray/SpinArray.m
@@ -235,51 +235,6 @@ classdef SpinArray < matlab.mixin.SetGet & matlab.mixin.Copyable
       warning('on', 'MATLAB:structOnObject')
     end
 
-    function target = target_Mss(obj, beta_iv, beta_ov, iv, ov, varargin)
-      %Compute target steady-state magnetization profile before alpha excitation
-      %INPUTS:
-      % - beta_iv (1,) deg, target iv beta flip angle
-      % - beta_ov (1,) deg, target ov beta flip angle
-      % - iv (*Nd) logical, inner volume mask
-      % - ov (*Nd) logical, outer volume mask
-      %OPTIONALS:
-      % - weight_iv (1,) [1.0]
-      % - weight_ov (1,) [1.0]
-      % - doEmbed [T/f]
-      % - alpha   (1,) deg [15]
-      % - TR      (1,) Sec [55e-3]
-      %OUTPUTS:
-      % - target struct with fields:
-      %     d      (nM, xyz) | (*Nd, xyz), target SS magnetization
-      %     weight (nM,)     | (*Nd,),    voxel weights
-      import attr.*
-
-      [arg.weight_iv, arg.weight_ov] = deal(1.0, 1.0);
-      [arg.doEmbed, arg.alpha, arg.TR] = deal(true, 15, 55e-3);
-      arg = attrParser(arg, varargin);
-
-      iv_ = obj.extract(iv);  % (nM, 1)
-      ov_ = obj.extract(ov);  % (nM, 1)
-
-      M0 = obj.M_(:, 3);      % (nM, 1), z-component
-      E1 = exp(-arg.TR ./ obj.T1_);  % (nM, 1)
-
-      beta_rad  = deg2rad(beta_iv .* iv_ + beta_ov .* ov_);  % (nM, 1)
-      alpha_rad = deg2rad(arg.alpha);
-
-      d_ = zeros(obj.nM, 3);
-      d_(:, 3) = (M0 .* (1 - E1)) ./ (1 - cos(beta_rad) .* cos(alpha_rad) .* E1) .* cos(beta_rad);
-      d_(:, 1) = (M0 .* (1 - E1)) ./ (1 - cos(beta_rad) .* cos(alpha_rad) .* E1) .* sin(beta_rad);
-      d_(isnan(d_)) = 0;
-
-      weight_ = arg.weight_iv .* ov_ + arg.weight_ov .* iv_;  % (nM, 1)
-
-      if arg.doEmbed
-        target = struct('d', obj.embed(d_), 'weight', obj.embed(weight_));
-      else
-        target = struct('d', d_, 'weight', weight_);
-      end
-    end
 
     function Mr_ = freeprec(obj, dur, varargin)
       %INPUTS:
